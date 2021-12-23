af69e5b5340ef56d0414ffdc37a7b06f4cfd6648
florence.aj <florence.aj@inapp.com>
2021-12-23 18:08:29 +0530

    Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './login';
+import Register from './register';
+import Welcome from './Welcome';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default function App () { 
+
+    return (
+      <Router>
+          <div>
+            <h2>Welcome to Login App</h2>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className="navbar-nav mr-auto">
+              <li>
+              Do you have an account? 
+              <Link to={'/login'} className="nav-link"> Login </Link></li>
+              <li>
+              New Here?
+              
+              <Link to={'/register'} className="nav-link">Register</Link></li>
+            </ul>
+            </nav>
+            <hr />
+            <Routes>
+                <Route exact path='/login' element={<Login/>}/>
+                <Route path='/register' element={<Register/>} />
+                <Route path='/welcome' element={<Welcome/>}/>
+            </Routes>
+      
+          </div>
+        </Router>
+      );
+    }
+  
+
+
---
+++ src/register.js
@@ -0,0 +1,141 @@
+import { Alert } from 'bootstrap';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+class Register extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name:'',
+      email:'',
+      phone:'',
+      password:'',
+      cpassword:'',
+      errors : {},
+      errMsg : {},
+    }
+  }
+
+
+
+
+  
+  formValidation = () => {
+   let mailFormat = /^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$/;
+   let phoneFormat = /^(\+\d{1,3}[- ]?)?\d{10}$/;
+
+      const {name, email, phone, password, cpassword} = this.state;
+      let isValid= true;
+      if (password !== cpassword) {
+        alert("Passwords don't match, Please try again");
+          isValid = false;
+      } 
+      if (password.length < 8){
+        alert("Password should countain min 8 characters");
+        isValid = false;
+
+      }
+      if(name.length<4)
+      {
+        alert("Name should contain minimum 4 characters");
+        isValid = false;
+
+      }
+      if(name.length>30)
+      {
+        alert("Name should not exceed 30 characters");
+        isValid = false;
+      }
+      if (mailFormat.match(email)){
+        isValid=true;
+      }else{
+        alert("Enter a valid Mail-ID")
+      }
+      if(phoneFormat.match(phone)){
+       isValid = true;
+      }else{
+        alert("Please enter a valid phone number")
+        isValid = false;
+      }
+      
+
+
+      
+  }
+
+
+  onChangeName = (e) =>{
+    this.setState({name:e.target.value})
+  }
+
+  onChangeEmail = (e) =>{
+    this.setState({email:e.target.value})
+  }
+
+  onChangePhone = (e) =>{
+    this.setState({phone:e.target.value})
+  }
+
+  onChangePassword = (e) =>{
+    this.setState({password:e.target.value})
+  }
+  onChangeCPassword = (e) =>{
+    this.setState({cpassword:e.target.value})
+  }
+
+  onSubmit = (e) =>{
+    const isValid=this.formValidation();
+    if(isValid)
+    {
+    let ob = {
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
+      password: this.state.password,
+      cpassword: this.state.cpassword,
+    }
+    let olddata = localStorage.getItem('formData');
+    if(olddata==null){
+      olddata = []
+      olddata.push(ob)
+      localStorage.setItem('formData', JSON.stringify(olddata));
+    }else{
+      let oldArr = JSON.parse(olddata)
+      oldArr.push(ob)
+      localStorage.setItem("formData", JSON.stringify(oldArr))
+      console.log(oldArr,'Abc')
+    }
+  }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <div className="form-group">
+          <label>Name</label>
+          <input type="text" className="form-control" value={this.state.name} onChange={this.onChangeName} required />
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input type="email" className="form-control" value={this.state.email} onChange={this.onChangeEmail} required />
+        </div>
+        <div className="form-group">
+          <label>Phone</label>
+          <input type="tel" className="form-control" value={this.state.phone} onChange={this.onChangePhone} required />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" className="form-control" value={this.state.password} onChange={this.onChangePassword} required />
+        </div>
+        <div className="form-group">
+          <label>Confirm Password</label>
+          <input type="password" className="form-control" value={this.state.cpassword} onChange={this.onChangeCPassword} required />
+        </div>
+        <button type="submit" className="btn btn-primary btn-block" onClick={this.props.onRegister}>Register</button>
+      </form>
+    )
+  }
+}
+
+export default Register;
